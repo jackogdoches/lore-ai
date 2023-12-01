1cc27bd312713811201edb05d04ef98ea0ff7d3e
jackogdoches <jack.whitmer@dal.ca>
2023-12-01 13:54:23 +0000

    Combined functions using new Boolean variable to check for Patron/Admin status. Refined command names and appended prompts.

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ loreAI = OpenAI()
 memory = {}
 useCount = {}
 
-def fetch_page_cheap(pageTitle, apiURL, author, memoryDict):
+def fetch_page(pageTitle, apiURL, author, memoryDict, hasPrivilege):
 	params = {
 		'action': 'query',
 		'format': 'json',
@@ -30,12 +30,18 @@ def fetch_page_cheap(pageTitle, apiURL, author, memoryDict):
 	pages = data.get('query', {}).get('pages', {})
 	page = next(iter(pages.values()))
 	content = page.get('extract', '')
-	appendedPrompt = "You are Lore, an AI wiki administration assistant for the Constructed Worlds Wiki. The user " + author + " has asked you to read a page. Here is the text content of that page: " + content + " (END PAGE CONTENT); Provide a brief summary in less than 1500 characters."
+	appendedPrompt = "You are Lore, an AI wiki administration assistant for the Constructed Worlds Wiki. The user " + author + " has asked you to read a page. Here is the text content of that page: " + content + " (END PAGE CONTENT); Provide a summary in less than 1999 characters (strict limit), being concise but as specific about details as possible."
 	messages = [{"role": "system", "content": appendedPrompt}]
-	response = loreAI.chat.completions.create(
-		model='gpt-3.5-turbo-1106',
-		messages=messages,
-	)
+	if hasPrivilege == True:
+		response = loreAI.chat.completions.create(
+			model='gpt-4-1106-preview',
+			messages=messages,
+		)
+	else:
+		response = loreAI.chat.completions.create(
+			model='gpt-3.5-turbo-1106',
+			messages=messages,
+		)
 	chatCompletion = response.choices[0].message.content
 	if author in memory:
 		retainMemory = memory[author]
@@ -44,58 +50,24 @@ def fetch_page_cheap(pageTitle, apiURL, author, memoryDict):
 		memory[author] = "THE FIRST MESSAGE FROM " + author + " WAS A REQUEST TO READ THE WIKI PAGE TITLED " + pageTitle + ", HERE IS THE SUMMARY YOU WROTE: " + chatCompletion
 	return chatCompletion
 
-def fetch_page(pageTitle, apiURL, author, memoryDict):
-	params = {
-		'action': 'query',
-		'format': 'json',
-		'titles': pageTitle,
-		'prop': 'extracts',
-		'exintro': True,
-		'explaintext': True,
-	}
-	response = requests.get(apiURL, params=params)
-	data = response.json()
-	pages = data.get('query', {}).get('pages', {})
-	page = next(iter(pages.values()))
-	content = page.get('extract', '')
-	appendedPrompt = "You are Lore, an AI wiki administration assistant for the Constructed Worlds Wiki. The user " + author + " has asked you to read a page. Here is the text content of that page: " + content + " (END PAGE CONTENT); Provide a brief summary in less than 1500 characters."
-	messages = [{"role": "system", "content": appendedPrompt}]
-	response = loreAI.chat.completions.create(
-		model='gpt-4-1106-preview',
-		messages=messages,
-	)
-	chatCompletion = response.choices[0].message.content
-	if author in memory:
-		retainMemory = memory[author]
-		memory[author] = retainMemory + " (END); THE FOLLOWING MESSAGE SENT BY YOU (LORE) IS A SUMMARY OF THE PAGE TITLED " + pageTitle + ": " + chatCompletion
+def return_message(prompt, author, memoryDict, hasPrivilege):
+	appended_prompt = "You are Lore, an AI wiki administration assistant for the Constructed Worlds Wiki. The wiki's technician, Fizzyflapjack, is your creator. The Constructed Worlds Wiki (commonly shortened as just Conworlds) is an independently-hosted worldbuilding, althistory, and general creative writing wiki. As of November 2023, the Bureaucrats of Conworlds are: Centrist16 (real name Justin) and Fizzyflapjack (real name Jack) (BOTH BUREAUCRATS ARE EQUAL IN POWER AND LEAD THE WIKI). The Administrators (sysops) of Conworlds are: T0oxi22, Andy Irons, and WorldMaker18. The following Discord user sent you a prompt: " + author  + " (END USERNAME); Here is a Python dictionary entry containing your message history with this user so far: " + memoryDict[author]  + " (END MESSAGE HISTORY); You have been given the following prompt to complete in STRICTLY 1499 characters or less, do your best to fulfil the request as literally as possible. Be concise with your answer but try and be specific with details: " + prompt + " (END PROMPT)"
+	messages = [{"role": "system", "content": appended_prompt}]
+	if hasPrivilege == True:
+		response = loreAI.chat.completions.create(
+			model='gpt-4-1106-preview',
+			messages=messages,
+			max_tokens=2000,
+		)
 	else:
-		memory[author] = "THE FIRST MESSAGE FROM " + author + " WAS A REQUEST TO READ THE WIKI PAGE TITLED " + pageTitle + ", HERE IS THE SUMMARY YOU WROTE: " + chatCompletion
-	return chatCompletion
-
-def return_message(prompt, author, memoryDict):
-	appended_prompt = "You are Lore, an AI wiki administration assistant for the Constructed Worlds Wiki. The wiki's technician, Fizzyflapjack, is your creator. The Constructed Worlds Wiki (commonly shortened as just Conworlds) is an independently-hosted worldbuilding, althistory, and general creative writing wiki. As of November 2023, the Bureaucrats of Conworlds are: Centrist16 (real name Justin) and Fizzyflapjack (real name Jack) (BOTH BUREAUCRATS ARE EQUAL IN POWER AND LEAD THE WIKI). The Administrators of Conworlds are: T0oxi22 (real name Toxi), Andy Irons (real name Andy), and WorldMaker18 (real name Liam). The following Discord user sent you a prompt:" + author  + "(END USERNAME); Here is a Python dictionary entry containing the messages that the user messaging you has sent you so far: " + memoryDict[author]  + "(END MESSAGE HISTORY); You have been given the following prompt to complete in 200 words or less, do your best to fulfil the request as literally as possible. Be concise with your answer and don't be too flowery: " + prompt + " (END PROMPT)"
-	messages = [{"role": "system", "content": appended_prompt}]
-	response = loreAI.chat.completions.create(
-		model='gpt-4-1106-preview',
-		messages=messages,
-		max_tokens=2000,
-	)
-	statementOut = response.choices[0].message.content
-	retainMemory = memory[author]
-	memory[author] = retainMemory + "(END); YOUR RESPONSE: " + statementOut
-	return statementOut
-
-def return_message_cheap(prompt, author, memoryDict):
-	appended_prompt = "You are Lore, an AI wiki administration assistant for the Constructed Worlds Wiki. The wiki's technician, Fizzyflapjack, is your creator. The Constructed Worlds Wiki (commonly shortened as just Conworlds) is an independently-hosted worldbuilding, althistory, and general creative writing wiki. As of November 2023, the Bureaucrats of Conworlds are: Centrist16 (real name Justin) and Fizzyflapjack (real name Jack) (BOTH BUREAUCRATS ARE EQUAL IN POWER AND LEAD THE WIKI). The Administrators of Conworlds are: T0oxi22 (real name Toxi), Andy Irons (real name Andy), and WorldMaker18 (real name Liam). The following Discord user sent you a prompt:" + author  + "(END USERNAME); Here is a Python dictionary entry containing the messages that the user messaging you has sent you so far: " + memoryDict[author]  + "(END MESSAGE HISTORY); You have been given the following prompt to complete in 200 words or less, do your best to fulfil the request as literally as possible. Be concise with your answer and don't be too flowery: " + prompt + " (END PROMPT)"
-	messages = [{"role": "system", "content": appended_prompt}]
-	response = loreAI.chat.completions.create(
+		response = loreAI.chat.completions.create(
 		model='gpt-3.5-turbo-1106',
 		messages=messages,
 		max_tokens=2000,
 	)
 	statementOut = response.choices[0].message.content
 	retainMemory = memory[author]
-	memory[author] = retainMemory + "(END); YOUR RESPONSE: " + statementOut
+	memory[author] = retainMemory + " (END); YOUR RESPONSE: " + statementOut
 	return statementOut
 
 
@@ -109,29 +81,31 @@ async def on_ready():
 
 @lore.event
 async def on_message(message):
-	if message.content.startswith("$lore"):
+	if message.content.startswith("$lore.chat"):
 		roleList = message.author.roles
 		roleNameList = []
 		for role in roleList:
 			roleNameList.append(role.name)
 		if "Administrator" in roleNameList or "Patron" in roleNameList:
+			hasPrivilege = True
 			if message.author.name in memory:
 				retain = memory[message.author.name]
 				concatenate = retain + "(END); NEXT MESSAGE FROM USER: " + message.content
 			else:
 				memory[message.author.name] = "FIRST MESSAGE FROM USER: " + message.content
 			lore_thinking = await message.channel.send("Thinking...")
-			returnMessage = return_message(message.content, message.author.name, memory)
+			returnMessage = return_message(message.content, message.author.name, memory, hasPrivilege)
 			await message.reply(returnMessage)
 			await lore_thinking.delete()
 		else:
+			hasPrivilege = False
 			if message.author.name in useCount:
 				if message.author.name in memory and useCount[message.author.name] <= 15:
 					retain = memory[message.author.name]
 					concatenate = retain + "(END); NEXT MESSAGE FROM USER: " + message.content
 					useCount[message.author.name] = useCount[message.author.name] + 1
 					lore_thinking = await message.channel.send("Thinking...")
-					returnMessage = return_message_cheap(message.content, message.author.name, memory)
+					returnMessage = return_message(message.content, message.author.name, memory, hasPrivilege)
 					await message.reply(returnMessage)
 					await lore_thinking.delete()
 				elif useCount[message.author.name] > 15:
@@ -139,7 +113,7 @@ async def on_message(message):
 				elif message.author.name not in memory and useCount[message.author.name] <= 15:
 					memory[message.author.name] = "FIRST MESSAGE FROM USER: " + message.content
 					lore_thinking = await message.channel.send("Thinking...")
-					returnMessage = return_message_cheap(message.content, message.author.name, memory)
+					returnMessage = return_message(message.content, message.author.name, memory, hasPrivilege)
 					await message.reply(returnMessage)
 					await lore_thinking.delete()
 			elif message.author.name not in useCount:
@@ -148,25 +122,25 @@ async def on_message(message):
 						retain = memory[message.author.name]
 						concatenate = retain + "(END); NEXT MESSAGE FROM USER: " + message.content
 						lore_thinking = await message.channel.send("Thinking...")
-						returnMessage = return_message_cheap(message.content, message.author.name, memory)
+						returnMessage = return_message(message.content, message.author.name, memory, hasPrivilege)
 						await message.reply(returnMessage)
 						await lore_thinking.delete()
 					else:
 						memory[message.author.name] = "FIRST MESSAGE FROM USER: " + message.content
 						lore_thinking = await message.channel.send("Thinking...")
-						returnMessage = return_message_cheap(message.content, message.author.name, memory)
+						returnMessage = return_message(message.content, message.author.name, memory, hasPrivilege)
 						await message.reply(returnMessage)
 						await lore_thinking.delete()
-	if message.content.startswith("$helplore"):
-		returnMessage = "At the moment, my only commands are '$lore', '$helplore', and '$wipelore'. Just use '$lore' at the beginning of your message and then ask me anything! Use '$wipelore' to clear my conversation history with you."
+	if message.content.startswith("$lore.help"):
+		returnMessage = "At the moment, my only commands are '$lore.chat', '$lore.help', '$lore.wipe', and '$lore.read'. To load page information into me, use '$lore.read' followed by exactly the name of the page as it appears on Conworlds (e.g., 'Sierra' will work but 'sierra' will not; NOTE: I am incapable of reading File: and Template: pages). Use '$lore.chat' at the beginning of a message and then ask me anything! Use '$lore.wipe' to clear my conversation history with you (it is recommended you do this somewhat frequently)."
 		await message.reply(returnMessage)
-	if message.content.startswith("$wipelore"):
+	if message.content.startswith("$lore.wipe"):
 		if message.author.name in memory:
 			memory[message.author.name] = ""
 			await message.reply("I have wiped my conversation history with " + message.author.name)
 		else:
 			await message.reply("There is nothing for me to wipe!")
-	if message.content.startswith("$purgelore"):
+	if message.content.startswith("$lore.purge"):
 		roleList = message.author.roles
 		roleNameList = []
 		for role in roleList:
@@ -180,23 +154,25 @@ async def on_message(message):
 				await message.channel.send("User counter purged!")
 		else:
 			await message.reply("I'm sorry, but only Administrators can use this command")
-	if message.content.startswith("$readlore"):
+	if message.content.startswith("$lore.read"):
 		roleList = message.author.roles
 		roleNameList = []
 		for role in roleList:
 			roleNameList.append(role.name)
 		if "Administrator" in roleNameList or "Patron" in roleNameList:
+			hasPrivilege = True
 			lore_thinking = await message.channel.send("Thinking...")
-			pageTitle = message.content[len("$readlore "):].strip()
+			pageTitle = message.content[len("$lore.read "):].strip()
 			apiURL = 'https://wiki.conworld.org/api.php'
-			pageRead = fetch_page(pageTitle, apiURL, message.author.name, memory)
+			pageRead = fetch_page(pageTitle, apiURL, message.author.name, memory, hasPrivilege)
 			await message.reply(pageRead)
 			await lore_thinking.delete()
 		else:
+			hasPrivilege = False
 			lore_thinking = await message.channel.send("Thinking...")
-			pageTitle = message.content[len("$readlore "):].strip()
+			pageTitle = message.content[len("$lore.read "):].strip()
 			apiURL = 'https://wiki.conworld.org/api.php'
-			pageRead = fetch_page_cheap(pageTitle, apiURL, message.author.name, memory)
+			pageRead = fetch_page(pageTitle, apiURL, message.author.name, memory, hasPrivilege)
 			await message.reply(pageRead)
 			await lore_thinking.delete()
 
